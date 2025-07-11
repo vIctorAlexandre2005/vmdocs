becc9df67a0032deb96271bc5bf2c3773a81afa1
victormarins <vtrm2005@gmail.com>
2025-07-11 08:36:09 -0300

    refactor(public): remove imagens desnecessarias e adiciona logo vm docs

--- src/Layout/layout.tsx
+++ src/Layout/layout.tsx
@@ -1,4 +1,4 @@
-import { Header } from "./header";
+import { Header } from "./Header";
 import { Main } from "./main";
 
 export default function LayoutApp({ children }: { children: React.ReactNode }) {
