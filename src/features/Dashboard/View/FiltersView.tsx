c8d8761275814378af3da6f06c3c8805d45d16de
vtrm2 <vtrm2005@gmail.com>
2025-07-27 11:06:12 -0300

    refactor(export excel): move logica para useTable

--- src/features/Dashboard/View/FiltersView.tsx
+++ src/features/Dashboard/View/FiltersView.tsx
@@ -4,47 +4,10 @@ import { IoFilter } from "react-icons/io5";
 import { TbSearch } from "react-icons/tb";
 import { PiExport } from "react-icons/pi";
 import { DataPdfProps } from "@/shared/contexts/UploadPdfContext";
-import { useCSVDownloader } from "react-papaparse";
-import * as XLSX from "xlsx";
-import { saveAs } from "file-saver";
+import { useTable } from "../viewModel/useTable";
 
 export function FiltersView({ dataPdf }: { dataPdf: DataPdfProps[] }) {
-  //const { CSVDownloader, Type } = useCSVDownloader();
-
-  function exportDataExcel(data: DataPdfProps[]) {
-    const exportData = data.map(({ file_name, collaborator, inc_req }) => ({
-    "Nome do termo": file_name,
-    Colaborador: collaborator,
-    "Incidente/Requisição": inc_req,
-  }));
-
-  const worksheet = XLSX.utils.json_to_sheet(exportData);
-
-  // Ajustar largura das colunas com base no conteúdo
-  const columnWidths = Object.keys(exportData[0]).map((key) => {
-    const maxLength = Math.max(
-      key.length,
-      ...exportData.map((row: any) => String(row[key]).length)
-    );
-    return { wch: maxLength + 2 };
-  });
-  worksheet["!cols"] = columnWidths;
-
-   const workbook = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(workbook, worksheet, "Termos");
-
-  const excelBuffer = XLSX.write(workbook, {
-    bookType: "xlsx",
-    type: "array",
-  });
-
-  const blob = new Blob([excelBuffer], {
-    type:
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8",
-  });
-
-  saveAs(blob, "termos.xlsx");
-  }
+  const { exportDataExcel } = useTable();
 
   return (
     <div className="flex items-center gap-2">
@@ -54,11 +17,11 @@ export function FiltersView({ dataPdf }: { dataPdf: DataPdfProps[] }) {
         iconInside={<TbSearch size={20} />}
         className="pb-2 pt-2 pl-2 w-sm placeholder:text-gray-600 bg-transparent border-gray-300 hover:border-indigo-400 placeholder:text-sm text-sm text-gray-800 focus:ring-1 focus:ring-indigo-500 rounded-full"
       />
-      {/* <ButtonComponent
+      <ButtonComponent
         text="Filtrar"
         className="w-32 text-sm text-gray-100 rounded-full bg-indigo-600"
         iconLeft={<IoFilter size={20} />}
-      /> */}
+      />
       <ButtonComponent
         text="Exportar .CSV"
         onClick={() => exportDataExcel(dataPdf)}
--- src/features/Dashboard/viewModel/useTable.tsx
+++ src/features/Dashboard/viewModel/useTable.tsx
@@ -1,5 +1,9 @@
-import { useUploadPdfContext } from "@/shared/contexts/UploadPdfContext";
-import { useEffect } from "react";
+import {
+  DataPdfProps,
+  useUploadPdfContext,
+} from "@/shared/contexts/UploadPdfContext";
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
 
 export function useTable() {
   const { dataPdf, pdfUrl, setPdfUrl } = useUploadPdfContext();
@@ -15,7 +19,44 @@ export function useTable() {
     setPdfUrl(url);
 
     return () => URL.revokeObjectURL(url);
-  };
+  }
 
-  return { dataPdf, pdfUrl, convertBase64ToPdf };
+  function exportDataExcel(data: DataPdfProps[]) {
+    const exportData = data.map(
+      ({ file_name, collaborator, inc_req, registration, created_at }) => ({
+        "Nome do arquivo": file_name,
+        "Colaborador": collaborator,
+        "Incidente/Requisição": inc_req,
+        "Matricula": registration,
+        "Data de criação": created_at,
+      })
+    );
+
+    const worksheet = XLSX.utils.json_to_sheet(exportData);
+
+    const columnWidths = Object.keys(exportData[0]).map((key) => {
+      const maxLength = Math.max(
+        key.length,
+        ...exportData.map((row: any) => String(row[key]).length)
+      );
+      return { wch: maxLength + 2 };
+    });
+    worksheet["!cols"] = columnWidths;
+
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Termos");
+
+    const excelBuffer = XLSX.write(workbook, {
+      bookType: "xlsx",
+      type: "array",
+    });
+
+    const blob = new Blob([excelBuffer], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8",
+    });
+
+    saveAs(blob, "termos.xlsx");
+  }
+
+  return { dataPdf, pdfUrl, convertBase64ToPdf, exportDataExcel };
 }
