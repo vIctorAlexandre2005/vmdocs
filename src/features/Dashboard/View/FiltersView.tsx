5ced416e3465908ad7593bc3298878b04d6e6e8d
vtrm2 <vtrm2005@gmail.com>
2025-07-27 10:38:42 -0300

    feat(excel): exporta dados com colunas alinhadas

--- src/features/Dashboard/View/FiltersView.tsx
+++ src/features/Dashboard/View/FiltersView.tsx
@@ -5,15 +5,46 @@ import { TbSearch } from "react-icons/tb";
 import { PiExport } from "react-icons/pi";
 import { DataPdfProps } from "@/shared/contexts/UploadPdfContext";
 import { useCSVDownloader } from "react-papaparse";
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
 
 export function FiltersView({ dataPdf }: { dataPdf: DataPdfProps[] }) {
-  const { CSVDownloader, Type } = useCSVDownloader();
+  //const { CSVDownloader, Type } = useCSVDownloader();
 
-  const exportData = dataPdf.map(({ file_name, collaborator, inc_req }) => ({
+  function exportDataExcel(data: DataPdfProps[]) {
+    const exportData = data.map(({ file_name, collaborator, inc_req }) => ({
     "Nome do termo": file_name,
     Colaborador: collaborator,
     "Incidente/Requisição": inc_req,
   }));
+
+  const worksheet = XLSX.utils.json_to_sheet(exportData);
+
+  // Ajustar largura das colunas com base no conteúdo
+  const columnWidths = Object.keys(exportData[0]).map((key) => {
+    const maxLength = Math.max(
+      key.length,
+      ...exportData.map((row: any) => String(row[key]).length)
+    );
+    return { wch: maxLength + 2 };
+  });
+  worksheet["!cols"] = columnWidths;
+
+   const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Termos");
+
+  const excelBuffer = XLSX.write(workbook, {
+    bookType: "xlsx",
+    type: "array",
+  });
+
+  const blob = new Blob([excelBuffer], {
+    type:
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8",
+  });
+
+  saveAs(blob, "termos.xlsx");
+  }
 
   return (
     <div className="flex items-center gap-2">
@@ -28,20 +59,12 @@ export function FiltersView({ dataPdf }: { dataPdf: DataPdfProps[] }) {
         className="w-32 text-sm text-gray-100 rounded-full bg-indigo-600"
         iconLeft={<IoFilter size={20} />}
       /> */}
-      {/* <ButtonComponent
+      <ButtonComponent
         text="Exportar .CSV"
+        onClick={() => exportDataExcel(dataPdf)}
         className=" w-52 text-center text-sm text-gray-100 rounded-full bg-indigo-600"
         iconLeft={<PiExport size={20} />}
-      /> */}
-
-      <CSVDownloader
-        type={Type.Button}
-        filename={"my_data_export"}
-        bom={true}
-        data={exportData}
-      >
-        Download CSV
-      </CSVDownloader>
+      />
     </div>
   );
 }
